571e11f50b46fd8d4947aacb79cd9fd64bbbd931
Kelly Phan <kelly@campus-026-096.ucdavis.edu>
2024-02-21 18:41:41 -0800

    tab and homepage

--- App.js
+++ App.js
@@ -1,75 +1,136 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Animated, useColorScheme} from 'react-native';
-import MaskedView from '@react-native-masked-view/masked-view';
-import Homepage from './screens/Homepage';
+import React, { useState, useEffect } from 'react';
+import { View, Animated, Image, Dimensions, TouchableOpacity, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const App = () => {
-  const [loadingProgress] = useState(new Animated.Value(0));
-  const [animationDone, setAnimationDone] = useState(false);
-  const theme = useColorScheme();
-  const isDarkMode = theme === 'dark';
+import HomeScreen from './screens/HomeScreen';
+import CreateReminderScreen from './screens/CreateReminderScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import Landing from './screens/Landing';
 
-  useEffect(() => {
-    Animated.timing(loadingProgress, {
-      toValue: 100,
-      duration: 1000,
-      useNativeDriver: true,
-      delay: 600,
-    }).start(() => {
-      setAnimationDone(true);
-    });
-  }, []);
+const Tab = createBottomTabNavigator();
+const screenWidth = Dimensions.get('window').width;
 
-  const darkLayer = <View style={[StyleSheet.absoluteFill, { backgroundColor: '#181823' }]} />;
-  const lightLayer = <View style={[StyleSheet.absoluteFill, { backgroundColor: '#EFF3F5' }]} />;
-  
-  const logoSource = isDarkMode ? require('./assets/geonotify-logo-dark.png') : require('./assets/geonotify-logo-light.png');
-
-  const imageScale = {
-    transform: [
-      {
-        scale: loadingProgress.interpolate({
-          inputRange: [0, 15, 100],
-          outputRange: [0.1, 0.05, 15],
-        }),
-      },
-    ],
-  };
-  const opacity = {
-    opacity: loadingProgress.interpolate({
-      inputRange: [0, 25, 50],
-      outputRange: [0, 0, 1],
-      extrapolate: 'clamp',
-    }),
-  };
-
+// Custom tab bar component
+const CustomTabBar = ({ state, descriptors, navigation }) => {
   return (
-    <View style={{ flex: 1 }}>
-      {isDarkMode ? lightLayer : darkLayer}
-      <MaskedView style={{ flex: 1 }} maskElement={
-        <View style={styles.centered}>
-          <Animated.Image
-            source={logoSource}
-            style={[{ width: 1000 }, imageScale]}
-            resizeMode="contain"
-          />
-        </View>
-      }>
-        {isDarkMode ? darkLayer : lightLayer}
-        <Animated.View style={[styles.centered, opacity]}>
-          <Homepage />
-        </Animated.View>
-      </MaskedView>
+    <View style={{flexDirection: 'row', backgroundColor: 'transparent', height: 80, width: screenWidth }}>
+      {/* Render tab bar background image */}
+      <Image
+        source={require('./assets/tab-bar-background.png')}
+        style={{ position: 'absolute', bottom: -40, left: 0, right: 0, width: screenWidth, height: 90, resizeMode: 'cover' }}
+      />
+      {/* Render individual tab buttons */}
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label = options.tabBarLabel !== undefined ? options.tabBarLabel : route.name;
+        const isFocused = state.index === index;
+
+        // Handle tab press
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        return (
+          <TouchableOpacity
+            key={index}
+            activeOpacity={0.8}
+            onPress={onPress}
+            style={[
+              { flex: 1, alignItems: 'center', justifyContent: 'center', marginTop: 50 },
+              route.name === 'Create Reminders' && {position: 'relative', bottom: 45 }
+            ]}
+            disabled={isFocused} // Disable touchable part if tab is active
+          >
+            {/* Render tab icon */}
+            <View style={{ alignItems: 'center' }}>
+              <Image
+                source={isFocused ? options.activeIcon : options.inactiveIcon}
+                style={[
+                  { width: 30, height: 30 },
+                  route.name === 'Create Reminders' && { width: 70, height: 70 }
+                ]}
+              />
+              {route.name !== 'Create Reminders' && <Text style={{ color: isFocused ? '#537FE7' : '#FFFFFF' , marginTop: 5 }}>{label}</Text>}
+            </View>
+          </TouchableOpacity>
+        );
+      })}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  centered: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const App = () => {
+  const [showContent, setShowContent] = useState(false); 
+  const opacity = useState(new Animated.Value(0))[0];
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContent(true);
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true,
+      }).start();
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <SafeAreaView style={{ flex: 1, marginBottom: 20 }}>
+      <Landing />
+      <Animated.View style={{ flex: 1, opacity }}>
+        {showContent && (
+          <NavigationContainer>
+            <Tab.Navigator
+              initialRouteName="Home"
+              tabBar={props => <CustomTabBar {...props} />}
+            >
+              <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  tabBarLabel: 'Home',
+                  headerShown: false,
+                  activeIcon: require('./assets/home-active.png'),
+                  inactiveIcon: require('./assets/home-inactive.png'),
+                }}
+              />
+              <Tab.Screen
+                name="Create Reminders"
+                component={CreateReminderScreen}
+                options={{
+                  tabBarLabel: 'Create Reminders',
+                  headerShown: false,
+                  activeIcon: require('./assets/create-icon.png'),
+                  inactiveIcon: require('./assets/create-icon.png'),
+                }}
+              />
+              <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                  tabBarLabel: 'Profile',
+                  headerShown: false,
+                  activeIcon: require('./assets/profile-active.png'),
+                  inactiveIcon: require('./assets/profile-inactive.png'),
+                }}
+              />
+            </Tab.Navigator>
+          </NavigationContainer>
+        )}
+      </Animated.View>
+    </SafeAreaView>
+  );
+};
 
 export default App;
---
+++ components/MapHomepage.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { View, Text, Dimensions, useColorScheme, Image } from 'react-native';
+import { darkHomepage } from '../styleSheets/dark/darkHomepage';
+import { lightHomepage } from '../styleSheets/light/lightHomepage';
+
+const screenWidth = Dimensions.get('window').width;
+
+const MapHomepage = () => {
+    const theme = useColorScheme();
+    const isDarkTheme = theme === 'dark';
+    const styles = isDarkTheme ? darkHomepage : lightHomepage;
+
+    return (
+        <View style={{ width: screenWidth }}>
+            <View style={[styles.mapFlex, { width: screenWidth }]}>
+                <View style={[styles.reminderFlex]}>
+                    <Image
+                        style={styles.logoMap}
+                        source={isDarkTheme ? require('../assets/home-logo-dark.png') : require('../assets/home-logo-light.png')}
+                    />
+                </View>
+                <View style={styles.reminderFlex}>
+                    <View style={styles.dashboard}>
+                        <View style={styles.dashboardFlex}>
+                            <Image
+                                style={styles.dashboardIcon}
+                                source={require('../assets/today-icon.png')}
+                                />
+                            <Text> 0 </Text>
+                        </View>
+                        <Text> Today</Text>
+                    </View>
+                    <View style={styles.dashboard}>
+                        <View style={styles.dashboardFlex}>
+                            <Image
+                                style={styles.dashboardIcon}
+                                source={require('../assets/scheduled-icon.png')}
+                                />
+                            <Text> 0 </Text>
+                        </View>
+                        <Text> Scheduled </Text>
+                    </View>
+                    <View style={styles.dashboard}>
+                        <View style={styles.dashboardFlex}>
+                            <Image
+                                style={styles.dashboardIcon}
+                                source={require('../assets/locations-icon.png')}
+                                />
+                            <Text> 0 </Text>
+                        </View>
+                        <Text> Locations</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+export default MapHomepage;
