f563acb53be6fc60c44692271c5c1d2618f6e627
Gavin Lang <gavincollislang@gmail.com>
2024-02-28 18:18:38 -0800

    got landing page animation merged

---
+++ screens/Landing.jsx
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Animated,
+  Image,
+  useColorScheme,
+  Dimensions,
+} from 'react-native';
+
+const Landing = () => {
+  const [loadingProgress] = useState(new Animated.Value(0));
+  const [animationDone, setAnimationDone] = useState(false);
+  const theme = useColorScheme();
+  const isDarkMode = theme === 'dark';
+
+  useEffect(() => {
+    Animated.timing(loadingProgress, {
+      toValue: 100,
+      duration: 1000,
+      useNativeDriver: true,
+      delay: 600,
+    }).start(() => {
+      setAnimationDone(true);
+    });
+  }, []);
+
+  const logoSource = isDarkMode
+    ? require('../assets/geonotify-logo-dark.png')
+    : require('../assets/geonotify-logo-light.png');
+
+  const imageScale = {
+    transform: [
+      {
+        scale: loadingProgress.interpolate({
+          inputRange: [0, 15, 90],
+          outputRange: [0.1, 0.05, 15],
+        }),
+      },
+    ],
+  };
+
+  return (
+    <View
+      style={[
+        StyleSheet.absoluteFill,
+        isDarkMode ? styles.lightLayer : styles.darkLayer,
+      ]}>
+      <View style={styles.centered}>
+        <Animated.Image
+          source={logoSource}
+          style={[{width: 1000}, imageScale]}
+          resizeMode="contain"
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  darkLayer: {
+    backgroundColor: '#181823',
+  },
+  lightLayer: {
+    backgroundColor: '#EFF3F5',
+  },
+});
+
+export default Landing;
--- screens/WelcomeScreen.jsx
+++ screens/WelcomeScreen.jsx
@@ -2,24 +2,27 @@ import {View, Text, Image, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
-
+import Landing from './Landing';
 const WelcomeScreen = () => {
   const navigation = useNavigation();
   return (
-    <View>
-      <SafeAreaView>
-        <View>
+    <>
+      <Landing />
+      <View>
+        <SafeAreaView>
           <View>
-            <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-              <Text>Sign Up</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
-              <Text>Login</Text>
-            </TouchableOpacity>
+            <View>
+              <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
+                <Text>Sign Up</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                <Text>Login</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-      </SafeAreaView>
-    </View>
+        </SafeAreaView>
+      </View>
+    </>
   );
 };
 
